07d4ab626d45c811b0ad854edecec5fc3cb7bbb8
Arnis Vuskans <ampatspell@gmail.com>
2020-10-31 19:06:46 +0200

    update zuglet api

--- client/app/models/user/uploads.js
+++ client/app/models/user/uploads.js
@@ -11,7 +11,7 @@ export default class Uploads extends EmberObject {
   @activate().content(({ store }) => store.collection('uploads').orderBy('createdAt', 'desc').query())
   query
 
-  @models('query.content').named('user/upload').mapping(doc => ({ doc }))
+  @models().source(({ query }) => query.content).named('user/upload').mapping(doc => ({ doc }))
   models
 
   async load() {
