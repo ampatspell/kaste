303c08d8840726e9a6a17e23b9d7af93f6e6de39
ampatspell <ampatspell@gmail.com>
2020-11-04 00:56:50 +0200

    google login

--- app/models/pages/session/new.js
+++ app/models/pages/session/new.js
@@ -1,9 +1,32 @@
-import SessionBase from './-base';
+import EmberObject from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
-export default class PagesSessioNNew extends SessionBase {
+export default class PagesSessionBase extends EmberObject {
 
-  async _perform(auth, email, password) {
-    await auth.methods.email.signIn(email, password);
+  @service
+  store
+
+  @tracked isBusy
+  @tracked error
+
+  async perform() {
+    if(this.isBusy) {
+      return;
+    }
+
+    this.error = null;
+    this.isBusy = true;
+
+    try {
+      await this.store.auth.methods.popup.google.signIn();
+      return true;
+    } catch(err) {
+      this.error = err;
+      return false;
+    } finally {
+      this.isBusy = false;
+    }
   }
 
 }
