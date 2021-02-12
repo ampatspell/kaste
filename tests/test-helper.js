fcfbe802fb6017a4e21a6f79cf5aa2e1e289d76e
ampatspell <ampatspell@gmail.com>
2021-02-12 18:52:18 +0200

    update deps

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,12 @@
-import Application from 'kaste/app';
-import config from 'kaste/config/environment';
+import Application from 'thingie/app';
+import config from 'thingie/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
