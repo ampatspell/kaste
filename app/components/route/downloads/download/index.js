37788de0abf4203134d4ac2fd622291df677ea61
ampatspell <ampatspell@gmail.com>
2024-05-03 18:03:57 +0300

    send screen views and download events to GA

--- app/components/route/downloads/download/index.js
+++ app/components/route/downloads/download/index.js
@@ -1,10 +1,14 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class RouteDownloadsDownloadIndexComponent extends Component {
 
+  @service analytics;
+
   @action
   open() {
+    this.analytics.onDownload(this.args.model.filename);
     window.open(this.args.model.url, 'top');
   }
 
